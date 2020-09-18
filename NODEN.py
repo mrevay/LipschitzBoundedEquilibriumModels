322e90af2a9d347287c618b43a2a5885431cb797
Max <mrev8802@uni.sydney.edu.au>
2020-09-18 10:37:04 +1000

    Current state trains an unstable unconstrained network =)

---
+++ NODEN.py
@@ -0,0 +1,150 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+class NODEN_SingleFc(nn.Module):
+    """ Simple MON linear class, just a single full multiply. """
+
+    def __init__(self, in_dim, out_dim, m=1.0):
+        super().__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        self.U = nn.Linear(in_dim, out_dim)
+        self.V = nn.Linear(out_dim, out_dim, bias=False)
+        self.S = nn.Linear(out_dim, out_dim, bias=False)
+
+        self.Lambda = nn.Parameter(torch.ones((out_dim)))
+        # self.Lambda = (torch.ones((out_dim)))
+        self.m = m
+        self.epsilon = 1E-5
+
+    def x_shape(self, n_batch):
+        return (n_batch, self.U.in_features)
+
+    def z_shape(self, n_batch):
+        return ((n_batch, self.V.in_features),)
+
+    def forward(self, x, *z):
+        self.Lambda.data[self.Lambda < self.epsilon] = self.epsilon
+        return (self.U(x) + self.multiply(*z)[0],)
+
+    def bias(self, x):
+        return (self.U(x),)
+
+    def multiply(self, *z):
+
+        z_out = z[0] @ self.W().T
+        return (z_out,)
+
+    def multiply_transpose(self, *g):
+        g_out = g[0] @ self.W()
+        return (g_out,)
+
+    def init_inverse(self, alpha, beta):
+        Id = torch.eye(self.V.weight.shape[0], dtype=self.V.weight.dtype,
+                      device=self.V.weight.device)
+
+        W = self.W()
+        self.Winv = torch.inverse(alpha * Id + beta * W)
+
+    def inverse(self, *z):
+        return (z[0] @ self.Winv.transpose(0, 1),)
+
+    def inverse_transpose(self, *g):
+        return (g[0] @ self.Winv,)
+
+    def W(self):
+
+        Id = torch.eye(self.V.weight.shape[0], dtype=self.V.weight.dtype,
+                       device=self.V.weight.device)
+        VTVz = self.V.weight.T @ self.V.weight + self.m * Id
+        S = self.S.weight
+        W = Id - self.Lambda.diag() @ (VTVz + S.T - S)
+
+        return W
+
+
+class NODEN_SingleFc_uncon(nn.Module):
+    """ Simple MON linear class, just a single full multiply. """
+
+    def __init__(self, in_dim, out_dim, m=1.0):
+        super().__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        self.U = nn.Linear(in_dim, out_dim)
+        V = nn.Linear(out_dim, out_dim, bias=False)
+        S = nn.Linear(out_dim, out_dim, bias=False)
+
+        Lambda = torch.ones((out_dim))
+
+        Id = torch.eye(V.weight.shape[0], dtype=V.weight.dtype,
+                       device=V.weight.device)
+        VTVz = V.weight.T @ V.weight + m * Id
+        S = S.weight
+
+        self.Wp = torch.nn.Parameter(Id - Lambda.diag() @ (VTVz + S.T - S))
+
+        self.m = m
+        self.epsilon = 1E-5
+
+    def x_shape(self, n_batch):
+        return (n_batch, self.U.in_features)
+
+    def z_shape(self, n_batch):
+        return ((n_batch, self.Wp.shape[1]),)
+
+    def forward(self, x, *z):
+        return (self.U(x) + self.multiply(*z)[0],)
+
+    def bias(self, x):
+        return (self.U(x),)
+
+    def multiply(self, *z):
+
+        z_out = z[0] @ self.W().T
+        return (z_out,)
+
+    def multiply_transpose(self, *g):
+        g_out = g[0] @ self.W()
+
+        return (g_out,)
+
+    def init_inverse(self, alpha, beta):
+        Id = torch.eye(self.Wp.shape[0], dtype=self.Wp.dtype,
+                      device=self.Wp.device)
+
+        W = self.W()
+
+        self.Winv = torch.inverse(alpha * Id + beta * W)
+
+    def inverse(self, *z):
+        return (z[0] @ self.Winv.transpose(0, 1),)
+
+    def inverse_transpose(self, *g):
+        return (g[0] @ self.Winv,)
+
+    def W(self):
+        return self.Wp
+
+class NODEN_ReLU(nn.Module):
+    def forward(self, *z):
+        return tuple(F.relu(z_) for z_ in z)
+
+    def derivative(self, *z):
+        return tuple((z_ > 0).type_as(z[0]) for z_ in z)
+
+
+class NDOEN_tanh(nn.Module):
+    def forward(self, *z):
+        print("Not implemented yet")
+        return None
+
+    def derivative(self, *z):
+        print("Not implemented yet")
+        return None
---
+++ scratch.py
@@ -0,0 +1,128 @@
+
+import matplotlib
+matplotlib.use("TkAgg")
+
+import splitting as sp
+import train
+
+import torch
+import numpy
+
+import NODEN
+import mon
+
+import scipy.io as io
+
+import matplotlib.pyplot as plt
+
+# torch.set_default_dtype(torch.float64)  # double precision for SDP
+# torch.set_printoptions(precision=4)
+
+if __name__ == "__main__":
+
+    # trainLoader, testLoader = train.mnist_loaders(train_batch_size=128,
+    #                                               test_batch_size=400)
+
+    # trainLoader, testLoader = train.cifar_loaders(train_batch_size=128, test_batch_size=400)
+    trainLoader, testLoader = train.cifar_loaders(train_batch_size=128, test_batch_size=400, augment=False)
+
+    epochs = 80
+    seed = 7
+    tol = 1E-3
+    width = 100
+    lr_decay_steps = 20
+
+    torch.manual_seed(seed)
+    numpy.random.seed(seed)
+
+    # unconstraIEND NETWORK
+    unconNet = train.NODENFcNet_uncon(sp.MONPeacemanRachford,
+                   in_dim=3*32*32,
+                   out_dim=width,
+                   alpha=1.0,
+                   max_iter=300,
+                   tol=tol,
+                   m=1.0)
+
+
+    uncon_train, uncon_test = train.train(trainLoader, testLoader,
+                unconNet,
+                max_lr=1e-3,
+                lr_mode='step',
+                step=lr_decay_steps,
+                change_mo=False,
+                epochs=epochs,    
+                print_freq=100,
+                tune_alpha=True)
+
+
+    # Ode stability condition
+    torch.manual_seed(seed)
+    numpy.random.seed(seed)
+
+    odeNet = train.NODENFcNet(sp.MONPeacemanRachford,
+                   in_dim=3*32*32,
+                   out_dim=width,
+                   alpha=1.0,
+                   max_iter=300,
+                   tol=tol,
+                   m=1.0)
+
+
+    ode_train, ode_test = train.train(trainLoader, testLoader,
+                odeNet,
+                max_lr=1e-3,
+                lr_mode='step',
+                step=lr_decay_steps,
+                change_mo=False,
+                epochs=epochs,    
+                print_freq=100,
+                tune_alpha=True)
+
+
+    # Monotone operator network
+    torch.manual_seed(seed)
+    numpy.random.seed(seed)
+
+    monNet = train.SingleFcNet(sp.MONPeacemanRachford,
+                              in_dim=3*32*32,
+                              out_dim=width,
+                              alpha=1.0,
+                              max_iter=300,
+                              tol=tol,
+                              m=1.0)
+
+    mon_train, mon_test = train.train(trainLoader, testLoader,
+                monNet,
+                max_lr=1e-3,
+                lr_mode='step',
+                step=lr_decay_steps,
+                change_mo=False,
+                epochs=epochs,
+                print_freq=100,
+                tune_alpha=True)
+
+    # X = torch.randn(100, 3*32*32)
+    # Z = torch.randn(100, width)
+
+    # mon = monNet.mon.linear_module
+    # ode = odeNet.mon.linear_module
+
+    # ode.init_inverse(1, -1)
+    # mon.init_inverse(1, -1)
+
+    # print("multiply is the same: ", (mon.multiply(Z)[0] - ode.multiply(Z)[0]).norm().item() < 1E-3)
+    # print("multiply_tranpose is the same: ", (mon.multiply_transpose(Z)[0] - ode.multiply_transpose(Z)[0]).norm().item() < 1E-3)
+    # print("inverse is the same: ", (mon.inverse(Z)[0] - ode.inverse(Z)[0]).norm().item() < 1E-3)
+    # print("inverse transpose is the same: ", (mon.inverse_transpose(Z)[0] - ode.inverse_transpose(Z)[0]).norm().item() < 1E-3)
+
+
+
+    p1, = plt.plot(ode_train)
+    p2, = plt.plot(mon_train)
+    p3, = plt.plot(uncon_train)
+    plt.legend([p1, p2, p3], ["ode", "mon", "uncon"])
+    # plt.yscale('log')
+    plt.show()
+
+    print('~fin~')
